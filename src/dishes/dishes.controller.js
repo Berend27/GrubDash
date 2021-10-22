d7fbf2fec3565b815e5d2aa8b7435d771c74d67d
Dexter Berend <dexterjberend@gmail.com>
2021-10-21 20:24:26 -0500

    dishes.controller read and list

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -1,3 +1,4 @@
+const notFound = require("../errors/notFound");
 const path = require("path");
 
 // Use the existing dishes data
@@ -6,4 +7,27 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
+function dishExists(req, res, next) {
+    const { dishId } = req.params;
+    const foundDish = dishes.find((dish) => dish.id === dishId);
+    if (foundDish) {
+        res.locals.dish = foundDish;
+        return next();
+    }
+    notFound(req, res, next);
+}
+
 // TODO: Implement the /dishes handlers needed to make the tests pass
+
+function list(req, res) {
+    res.json({ data: dishes });
+}
+
+function read(req, res) {
+    res.json({ data: res.locals.dish });
+}
+
+module.exports = {
+    list,
+    read: [dishExists, read]
+}
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,5 +1,12 @@
+const controller = require("./dishes.controller");
 const router = require("express").Router();
 
 // TODO: Implement the /dishes routes needed to make the tests pass
+router
+    .route("/")
+    .get(controller.list);
+router
+    .route("/:dishId")
+    .get(controller.read);
 
 module.exports = router;
